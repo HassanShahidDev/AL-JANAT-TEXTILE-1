031698d3236b6d8726ed8b1dbdd7e38f14ecb14d
HassanShahidDev <hassanshahid778866@gmail.com>
2026-01-29 14:44:10 +0500

    Fix image path case

--- src/pages/About.jsx
+++ src/pages/About.jsx
@@ -1,5 +1,5 @@
 import useSEO from "../hooks/useSEO"
-import aboutImg from "../assets/images/about.jpg"
+import aboutImg from "../assets/images/about.Jpg"; // capital J
 import ownerImg from "../assets/images/owner.jpg"
 
 export default function About() {
